533190b7bbf3596c3163b6818bb46095caa8dee0
chenck <244926053@qq.com>
2020-10-03 21:20:03 +0800

    去掉无用的import引入

--- l2cache-core/src/main/java/com/coy/l2cache/cache/RedissonRBucketCache.java
+++ l2cache-core/src/main/java/com/coy/l2cache/cache/RedissonRBucketCache.java
@@ -5,11 +5,12 @@ import com.coy.l2cache.consts.CacheType;
 import com.coy.l2cache.content.NullValue;
 import com.coy.l2cache.exception.RedisTrylockFailException;
 import com.coy.l2cache.util.SpringCacheExceptionUtil;
-import com.sun.javaws.exceptions.InvalidArgumentException;
-import org.redisson.api.*;
+import org.redisson.api.RBucket;
+import org.redisson.api.RLock;
+import org.redisson.api.RMap;
+import org.redisson.api.RedissonClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.util.StringUtils;
 
 import java.util.concurrent.Callable;
 import java.util.concurrent.TimeUnit;
