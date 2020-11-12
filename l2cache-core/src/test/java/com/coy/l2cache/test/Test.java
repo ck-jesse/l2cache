a6fe42b9331e2bbda2a62959e93abf9a4982d788
WG-chenchuangkun <cck1112@163.com>
2020-11-12 21:51:19 +0800

    去掉provider的scope

--- l2cache-core/src/test/java/com/coy/l2cache/test/Test.java
+++ l2cache-core/src/test/java/com/coy/l2cache/test/Test.java
@@ -1,5 +1,10 @@
 package com.coy.l2cache.test;
 
+import org.redisson.Redisson;
+import org.redisson.api.BatchResult;
+import org.redisson.api.RBatch;
+import org.redisson.api.RedissonClient;
+
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
 
@@ -18,7 +23,22 @@ public class Test {
         System.out.println(queue);
         queue.add(2);
         System.out.println(queue);
-        queue.add(3);
-        System.out.println(queue);
+
+
+        RedissonClient redissonClient = Redisson.create();
+
+        RBatch batch = redissonClient.createBatch();
+        for (int i = 0; i < 50; i++) {
+            String tempKey = "key" + i;
+            String value = "value" + i;
+            batch.getBucket(tempKey).setAsync(value);
+            System.out.println("key=" + tempKey + ",value=" + value);
+            if (i != 0 && i % 10 == 0) {
+                BatchResult result = batch.execute();
+                System.out.println("i=" + i + ",SyncedSlaves=" + result.getSyncedSlaves() + ",Responses=" + result.getResponses());
+            }
+        }
+        BatchResult result = batch.execute();
+        System.out.println("SyncedSlaves=" + result.getSyncedSlaves() + ",Responses=" + result.getResponses());
     }
 }
