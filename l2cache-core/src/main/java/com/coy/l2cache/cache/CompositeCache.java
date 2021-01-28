62b92c5fd093be2c686fcf0fbbb8e4ff139e41bc
WG-zengjucai <zengjucai@weeget.cn>
2021-01-28 17:08:23 +0800

    CompositeCache batchGet方法

--- l2cache-core/src/main/java/com/coy/l2cache/cache/CompositeCache.java
+++ l2cache-core/src/main/java/com/coy/l2cache/cache/CompositeCache.java
@@ -3,6 +3,7 @@ package com.coy.l2cache.cache;
 import com.coy.l2cache.Cache;
 import com.coy.l2cache.CacheConfig;
 import com.coy.l2cache.consts.CacheType;
+import com.google.common.collect.Maps;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.util.CollectionUtils;
@@ -10,6 +11,7 @@ import org.springframework.util.CollectionUtils;
 import java.util.*;
 import java.util.concurrent.Callable;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Function;
 
 /**
  * 组合缓存器
@@ -179,6 +181,22 @@ public class CompositeCache extends AbstractAdaptingCache implements Cache {
      * @return
      */
     private boolean ifL1Open(Object key) {
+        // 检测开关与缓存名称
+        if (ifL1Open()) {
+            return true;
+        }
+
+        // 检测key
+        return ifL1OpenByKey(key);
+    }
+
+
+    /**
+     * 本地缓存检测，检测开关与缓存名称
+     *
+     * @return
+     */
+    private boolean ifL1Open() {
         // 判断是否开启过本地缓存
         if (composite.isL1AllOpen() || composite.isL1Manual()) {
             openedL1Cache.compareAndSet(false, true);
@@ -194,6 +212,19 @@ public class CompositeCache extends AbstractAdaptingCache implements Cache {
             if (!CollectionUtils.isEmpty(l1ManualCacheNameSet) && composite.getL1ManualCacheNameSet().contains(this.getCacheName())) {
                 return true;
             }
+        }
+        return false;
+    }
+
+    /**
+     * 本地缓存检测，检测key
+     *
+     * @param key
+     * @return
+     */
+    private boolean ifL1OpenByKey(Object key) {
+        // 是否使用手动匹配开关
+        if (composite.isL1Manual()) {
             // 手动匹配缓存key集合，针对单个key维度
             Set<String> l1ManualKeySet = composite.getL1ManualKeySet();
             if (!CollectionUtils.isEmpty(l1ManualKeySet) && l1ManualKeySet.contains(buildKeyBase(key))) {
@@ -255,5 +286,56 @@ public class CompositeCache extends AbstractAdaptingCache implements Cache {
             l2ResultMap.forEach((key, value) -> resultMap.put(key, (T) value));
         }
         return resultMap;
+    }
+
+    @Override
+    public <K, V> Map<K, V> batchGet(List<K> keyList, Function<Object, K> keyBuilder) {
+        return this.batchGetOrLoad(keyList, keyBuilder, null);
+    }
+
+    @Override
+    public <K, V> Map<K, V> batchGetOrLoad(List<K> keyList, Function<Object, K> keyBuilder, Function<List<K>, Map<K, V>> valueLoader) {
+        // 将keyList 转换为cacheKey，因K可能是自定义DTO，结构 <K, cacheKey>
+        Map<K, Object> keyMap = new HashMap<>();
+        keyList.forEach(key -> keyMap.put(key, keyBuilder.apply(key)));
+
+        // 获取到一级缓存中查询的key
+        Map<K, Object> l1KeyMap = new HashMap<>();
+        if (ifL1Open()) {
+            l1KeyMap.putAll(keyMap);
+        } else {
+            keyMap.entrySet().stream().filter(entry -> ifL1OpenByKey(entry.getValue())).forEach(entry -> l1KeyMap.put(entry.getKey(), entry.getValue()));
+        }
+
+        // 缓存命中列表
+        Map<K, V> hitMap = Maps.newHashMap();
+        // 未命中列表
+        Map<K, Object> notHitKeyMap = new HashMap<>();
+
+        // 一级缓存批量查询
+        if (!CollectionUtils.isEmpty(l1KeyMap)) {
+            Map<K, V> l1HitMap = level1Cache.batchGetOrLoad(keyList, keyBuilder, valueLoader);
+            hitMap.putAll(l1HitMap);
+            // 获取未命中列表
+            keyMap.entrySet().stream().filter(entry -> !l1HitMap.containsKey(entry.getKey())).forEach(entry -> notHitKeyMap.put(entry.getKey(), entry.getValue()));
+            logger.info("[CompositeCache] batchGetOrLoad l1Cache, cacheName={}, l1KeyMap={}, l1HitMap={},  l1NotHitMap={}", this.getCacheName(), l1KeyMap, hitMap, notHitKeyMap);
+        } else {
+            // 获取未命中列表
+            notHitKeyMap.putAll(keyMap);
+            logger.info("[CompositeCache] batchGetOrLoad l1Cache not hit, cacheName={}, notHitKeyMap={}", this.getCacheName(), l1KeyMap);
+        }
+
+        // 一级缓存全部命中
+        if (CollectionUtils.isEmpty(notHitKeyMap)) {
+            logger.info("[CompositeCache] batchGetOrLoad l1Cache all hit, cacheName={}, KeyMap={}, l1HitMap={}", this.getCacheName(), keyMap, hitMap);
+            return hitMap;
+        }
+
+        // 二级缓存批量查询
+        Map<K, V> l2HitMap = level2Cache.batchGetOrLoad(new ArrayList<>(notHitKeyMap.keySet()), keyBuilder, valueLoader);
+        logger.info("[CompositeCache] batchGetOrLoad l2Cache, cacheName={}, l2KeyMap={}, l2HitMap={}", this.getCacheName(), notHitKeyMap.keySet(), l2HitMap);
+        hitMap.putAll(l2HitMap);
+        logger.info("[CompositeCache] batchGetOrLoad, cacheName={}, KeyMap={}, keyMapSize={}, hitMap={}, hitMapSize={}", this.getCacheName(), keyMap, keyMap.size(), hitMap, hitMap.size());
+        return hitMap;
     }
 }
